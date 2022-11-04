31886e9f3945c7510978e04730b90d8d9aff7c94
DavidMeagher1 <meagherjdavid@gmail.com>
2022-11-03 21:48:53 -0700

    removed install

--- walli-install.bat
+++
@@ -1,9 +0,0 @@
-:: https://mirrors.kernel.org/gnu/emacs/windows/emacs-28/emacs-28.2-installer.exe
-set emacsd="%HOMEPATH%\AppData\Roaming\.emacs.d"
-
-curl "https://mirrors.kernel.org/gnu/emacs/windows/emacs-28/emacs-28.2-installer.exe" --output-dir "%HOMEPATH%/Downloads/"
-
-start /wait "%HOMEPATH%/Downloads/emacs-28.2-installer.exe"
-
-cp "%HOMEPATH%\Documents\GitHub\dot\init.el" "%emacsd%\init.el"
-cp "%HOMEPATH%\Documents\GitHub\dot\init.org" "%emacsd%\init.el"

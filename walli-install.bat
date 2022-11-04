d62cbf7761d4c154e134ed9c47d19cb5a63f89f8
DavidMeagher1 <meagherjdavid@gmail.com>
2022-11-03 21:45:57 -0700

    fixed install

--- walli-install.bat
+++ walli-install.bat
@@ -1,9 +1,9 @@
 :: https://mirrors.kernel.org/gnu/emacs/windows/emacs-28/emacs-28.2-installer.exe
 set emacsd="%HOMEPATH%\AppData\Roaming\.emacs.d"
 
-curl "https://mirrors.kernel.org/gnu/emacs/windows/emacs-28/emacs-28.2-installer.exe" --output-dir "~/Downloads/"
+curl "https://mirrors.kernel.org/gnu/emacs/windows/emacs-28/emacs-28.2-installer.exe" --output-dir "%HOMEPATH%/Downloads/"
 
-start /wait "~/Downloads/emacs-28.2-installer.exe"
+start /wait "%HOMEPATH%/Downloads/emacs-28.2-installer.exe"
 
-cp ".\init.el" "%emacsd%\init.el"
-cp ".\init.org" "%emcasd%\init.el"
+cp "%HOMEPATH%\Documents\GitHub\dot\init.el" "%emacsd%\init.el"
+cp "%HOMEPATH%\Documents\GitHub\dot\init.org" "%emacsd%\init.el"
